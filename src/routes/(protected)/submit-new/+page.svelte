00ebccf78f0522f1b53b43e3162bc309a9f1b4b2
Naglis <naglis1134@gmail.com>
2024-05-31 01:12:15 +0300

    createnew details

--- src/routes/(protected)/submit-new/+page.svelte
+++ src/routes/(protected)/submit-new/+page.svelte
@@ -20,8 +20,16 @@
 	<p>Welcome, {user.username}</p>
 	<p>If you refresh, changes will not be saved</p>
 	<p><a href="/submit-dashboard">Go back</a> (not saved)</p>
-	<p>Submit new</p>
-
+	<p>category1</p>
+	<p>categoryName</p>
+	<p>description</p>
+	<p>q1</p>
+	<p>questionText/image</p>
+	<p>answerText/image</p>
+	<p>q2</p>
+	<p>questionText/image</p>
+	<p>answerText/image</p>
+	<p>+</p>
 	<p>
 		Upload only original problems, created by you (which will wave your copyright rights and make it
 		royalty-free) or problems that are already royalty-free. It will be reviewed by admins and if
