a2620715ce53f00da7144925bfb0e5f9326c6188
Naglis <naglis1134@gmail.com>
2024-07-08 09:48:06 +0300

    Lazy loading front page

--- src/components/layout/CategoryWithProblems.svelte
+++ src/components/layout/CategoryWithProblems.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
 	import ProblemComponent from '$components/ui/ProblemComponent.svelte';
-	import { problemApi, problemOldApi } from '$services/apiService';
+	import { problemOldApi } from '$services/apiService';
 	import type { Category, ProblemDisplayViewDto } from '$services/gen-client';
 	import { Accordion, AccordionItem } from 'flowbite-svelte';
-	import { onMount } from 'svelte';
-
-	let loading: boolean = true;
-	let error: string | null = null;
-	let problems: ProblemDisplayViewDto[] = [];
+	import { writable } from 'svelte/store';
 
 	export let category: Category;
 
-	onMount(async () => {
+	let problems: ProblemDisplayViewDto[] = [];
+	let isLoaded = writable(false);
+	let isOpen = false;
+
+	$: if (isOpen) {
+		loadProblems();
+	}
+
+	async function loadProblems() {
+		if (!isOpen || $isLoaded) return;
 		const response = await problemOldApi.getPublicProblemsByCategory(category.id);
 		problems = response.data;
-	});
+		isLoaded.set(true);
+	}
 </script>
 
 <Accordion>
-	<AccordionItem class="bg-slate-200 my-4">
+	<AccordionItem bind:open={isOpen} class="bg-slate-200 my-4">
 		<span slot="header" class="text-black">{category.name}</span>
-		{#if loading}
-			<p>Kraunasi...</p>
-		{:else if error}
-			<p>Klaida: {error}</p>
-		{:else}
-			<div class="container mx-auto">
-				{#each Object.entries(problems) as [id, problem]}
-					<div class="my-3">
-						<ProblemComponent
-							problemMainData={{
-								skfCode: problem.skfCode,
-								problemText: problem.problemText,
-								problemImageSrc: problem.problemImageSrc,
-								answerText: problem.answerText,
-								answerImageSrc: problem.answerImageSrc
-							}}
-							problemMetaData={{
-								author: problem.author,
-								categoryName: category.name,
-								source: 'Dar neįgyvendinta...'
-							}}
-						/>
-					</div>
-				{/each}
-			</div>
-		{/if}
+		<div class="container mx-auto">
+			{#each problems as problem (problem.id)}
+				<div class="my-3">
+					<ProblemComponent
+						problemMainData={{
+							skfCode: problem.skfCode,
+							problemText: problem.problemText,
+							problemImageSrc: problem.problemImageSrc,
+							answerText: problem.answerText,
+							answerImageSrc: problem.answerImageSrc
+						}}
+						problemMetaData={{
+							author: problem.author,
+							categoryName: category.name,
+							source: 'Dar neįgyvendinta...'
+						}}
+					/>
+				</div>
+			{/each}
+		</div>
 	</AccordionItem>
 </Accordion>
