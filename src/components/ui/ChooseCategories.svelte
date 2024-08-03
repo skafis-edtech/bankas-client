36e02bf74f541b2146aeff9024c6f9825438dc4b
Naglis <naglis.suliokas@gmail.com>
2024-08-03 10:07:57 +0300

    Multiselect logic works

--- src/components/ui/ChooseCategories.svelte
+++ src/components/ui/ChooseCategories.svelte
@@ -2,7 +2,7 @@
 	import { successStore } from '$lib/stores';
 	import { sortApi } from '$services/apiService';
 	import type { Category } from '$services/gen-client';
-	import { Button } from 'flowbite-svelte';
+	import { Button, Checkbox } from 'flowbite-svelte';
 
 	export let categories: Category[] = [];
 	export let currentProblemId: string;
@@ -10,9 +10,14 @@
 	export let skipProblem: () => void;
 
 	let selectedCategories: string[] = [];
+	let isConfirmNeeded: boolean = false;
 
 	$: {
 		selectedCategories = currentProblemCategories;
+	}
+
+	$: {
+		isConfirmNeeded = currentProblemCategories.length > 0 || selectedCategories.length > 0;
 	}
 
 	function toggleCategory(categoryId: string) {
@@ -33,19 +38,35 @@
 <div
 	class="flex fixed top-16 left-0 w-72 z-10 gap-4 flex-wrap bg-black p-4 rounded-md overflow-scroll h-[calc(100%-4rem)]"
 >
+	<Button size="sm" color="yellow" on:click={skipProblem}>PRALEISTI</Button>
+	{#if isConfirmNeeded}
+		<Button size="sm" color="red" on:click={sortProblem}>ĮRAŠYTI</Button>
+	{/if}
 	{#each categories as category}
 		<Button
 			size="sm"
 			color={selectedCategories.includes(category.id) ? 'blue' : 'light'}
-			on:click={() => toggleCategory(category.id)}
-			>{category.name}
+			on:click={() => {
+				if (isConfirmNeeded) {
+					alert(
+						'Yra pažymėtų kategorijų, dabar naudokitės žymėjimo langeliais ir mygtuku "ĮRAŠYTI"'
+					);
+				} else {
+					selectedCategories = [category.id];
+					sortProblem();
+				}
+			}}
+			>{category.name}<Checkbox
+				on:click={(event) => {
+					event.stopPropagation();
+					toggleCategory(category.id);
+				}}
+				checked={selectedCategories.includes(category.id)}
+				class="h-6 w-6"
+			/>
 		</Button>
 	{/each}
-	{#if currentProblemCategories === selectedCategories}
-		<Button size="sm" color="red" on:click={skipProblem}>PRALEISTI</Button>
-	{:else}
-		<Button size="sm" color="red" on:click={sortProblem}>ĮRAŠYTI</Button>
-	{/if}
+
 	<p class="text-white w-fit">
 		Nerandate tinkamos kategorijos? Susisiekite el. paštu naglis.suliokas@gmail.com
 	</p>
