4a00c4ad6865cac4cd015dee96fc1cf2d6395c2c
naglissul <naglis1134@gmail.com>
2024-10-05 06:37:45 +0200

    prod link fix

--- src/services/gen-client/base.ts
+++ src/services/gen-client/base.ts
@@ -18,7 +18,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = 'http://localhost:9000'.replace(/\/+$/, '');
+export const BASE_PATH = 'https://api2.skafis.com'.replace(/\/+$/, '');
 
 /**
  *
